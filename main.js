fe5d3141dc284926e1083386d1ff73dd5516d396
Apidcloud <luisapidcloud@hotmail.com>
2018-01-17 17:17:08 +0000

    feat: add working generation example with prebuilt package

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 /**
  * Created by Luis on 05/01/2017.
  */
-
-var generateBMFont = require('msdf-bmfont');
-var fs = require('fs');
+const generateBMFont = require('msdf-bmfont-xml');
+const fs = require('fs');
 
 const font = "OpenSans-Regular.ttf";
 
+const opt = {
+    outputType: 'json'
+  };
+
 generateBMFont('fnt/' + font, (error, textures, font) => {
-    if (error)
-    throw error;
-textures.forEach((sheet, index) => {
-    font.pages.push(`sheet${index}.png`);
-fs.writeFile(`generated/sheet${index}.png`, sheet, (err) => {
-    if (err)
-    throw err;
-});
-});
-fs.writeFile('generated/font.json', JSON.stringify(font), (err) => {
+  if (error) throw error;
+  textures.forEach((texture, index) => {
+    fs.writeFile("generatedPreBuilt/" + `sheet${index}.png`, texture.texture, (err) => {
+      if (err) throw err;
+    });
+  });
+  fs.writeFile("generatedPreBuilt/font.json", font.data, (err) => {
     if (err) throw err;
-});
+  });
 });
